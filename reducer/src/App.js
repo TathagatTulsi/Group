042c7e4697b77f91312a556e3ea84f0c10c60498
Tathagat Tulsi <81804350+TathagatTulsi@users.noreply.github.com>
2023-07-27 15:18:10 +0530

    Add files via upload

--- reducer/src/App.js
+++ reducer/src/App.js
@@ -1,21 +1,22 @@
 import React, { useReducer, useState } from 'react'
 
+//switch case
 const reducer = (state, action) => {
 
   switch (action.type) {
     case "INCREMENT":
       return { ...state, count: state.count + 1 }
 
-    case "Decrement":
+    case "DECREMENT":
       return { ...state, count: state.count - 1 }
 
-    case "AddNumber":
+    case "ADDNUMBER":
       return { ...state, count: state.count + +action.payload }
 
-    case "SubNumber":
+    case "SUBNUMBER":
       return { ...state, count: state.count - +action.payload }
-      
-      case "RESET":
+
+    case "RESET":
       return { ...state, count: 0 }
   }
 }
@@ -23,22 +24,22 @@ const reducer = (state, action) => {
 function App() {
   const [state, dispatch] = useReducer(reducer, { count: 0 })
   const [num, setNum] = useState(0)
-  
+
   return (
     <div>
       <h1>{state.count}</h1>
 
       <div>
         <input type="number" value={num} onChange={(e) => setNum(e.target.value)}></input>
-        <button onClick={() => dispatch({ type: "AddNumber", payload: num })}>Add</button>
-        <button onClick={() => dispatch({ type: "SubNumber", payload: num })}>substract</button>
+        <button onClick={() => dispatch({ type: "ADDNUMBER", payload: num })}>Add</button>
+        <button onClick={() => dispatch({ type: "SUBNUMBER", payload: num })}>substract</button>
 
       </div>
 
 
       <div>
         <button onClick={() => dispatch({ type: "INCREMENT" })} >Increment</button>
-        <button onClick={() => dispatch({ type: "Decrement" })} >Decrement</button>
+        <button onClick={() => dispatch({ type: "DECREMENT" })} >Decrement</button>
         <button onClick={() => dispatch({ type: "RESET" })} >RESET</button>
 
 
